bf7df766e60c323d1d31aad4856520714bd4a287
AkshayaPanuganti02 <89069446+AkshayaPanuganti02@users.noreply.github.com>
2021-08-17 21:57:39 +0530

    Assignment-1 submitted

---
+++ Python assignment day1.py
@@ -0,0 +1,5 @@
+# Take two inputs and store in variables x and y, find x to the power y.
+x=int(input("Enter any postive integer x : "))
+y=int(input("Enter any positive integer y : "))
+power=pow(x,y)
+print("{0} power {1}={2}".format(x,y,power))
